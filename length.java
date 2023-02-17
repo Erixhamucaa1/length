dde4a4f2c3d2da63a01094c45074fef8f71c78aa
Erixhamucaa1 <122046005+Erixhamucaa1@users.noreply.github.com>
2023-02-18 00:14:29 +0100

    Add files via upload

---
+++ length.java
@@ -0,0 +1,14 @@
+package length;
+
+public class length {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String emri = "une mesoj java";
+		System.out.println(emri.length());
+		
+	
+		
+	}
+
+}
